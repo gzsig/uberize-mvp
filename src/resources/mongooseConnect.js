79cff3db66f0745c396772dcd32e04e3e2134c4e
Gabriel Zsigmond <gaazsig@gmail.com>
2020-01-30 15:22:15 -0300

    API config

---
+++ src/resources/mongooseConnect.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+MONGOCONNECTSTR = process.env.MONGOCONNECTSTR;
+
+function mongooseConnect() {
+  mongoose
+  .connect(`${MONGOCONNECTSTR}`,{
+    // userUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then((x)=>{
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+  })
+  .catch((err) => {
+    console.error('An error accured while trying to connect to DB', err)
+  })
+}
+
+module.exports = {
+  mongooseConnect,
+}
