89bdfe385cc4bfc56f4ab6f70c05b677cc69bf35
Gabriel Zsigmond <gaazsig@gmail.com>
2020-02-05 19:49:27 -0300

    added jwt to get user info from id_token

--- api/src/config/googleConfig.js
+++ api/src/config/googleConfig.js
@@ -1,4 +1,5 @@
 const { google } = require('googleapis')
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 
 const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env
@@ -36,10 +37,38 @@ const urlGoogle = () => {
   return url
 }
 
- const getTokens = async (code) => {
+const getTokens = async (code) => {
   const auth = createConnection();
-  const tokens = await auth.getToken(code)
-  console.log(tokens);
+  const { tokens } = await auth.getToken(code)
+  auth.setCredentials(tokens);
+  const calendar = google.calendar({ version: 'v3', auth });
+  calendar.events.list({
+    calendarId: 'primary',
+    timeMin: (new Date()).toISOString(),
+    maxResults: 10,
+    singleEvents: true,
+    orderBy: 'startTime',
+  }, (err, res) => {
+    if (err) return console.log('The API returned an error: ' + err);
+    const events = res.data.items;
+    if (events.length) {
+      console.log('Upcoming 10 events:');
+      events.map((event, i) => {
+        const start = event.start.dateTime || event.start.date;
+        console.log(`${start} - ${event.summary}`);
+      });
+    } else {
+      console.log('No upcoming events found.');
+    }
+  });
+
+  console.log('TOKENS:', tokens);
+  const { id_token } = tokens;
+  console.log('id_token:', id_token);
+
+  const decoded = jwt.decode(id_token)
+  console.log(decoded);
+
 }
 
 module.exports = {
