cf9fcbb41087edd4c8d16b7cff772bb166d859f6
Gabriel Zsigmond <gaazsig@gmail.com>
2020-02-10 18:04:30 -0300

    using cors to avoid conflicts with client

--- lendario/lendario-api/src/app.js
+++ lendario/lendario-api/src/app.js
@@ -1,6 +1,7 @@
 // Requiring necessary packages
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require('cors')
 // Requiring passport as we've configured it
 const passport = require("passport");
 // Setting up port 
@@ -15,6 +16,13 @@ app.use(bodyParser.json());
 // app.use(express.static("client/build"));
 app.use(passport.initialize());
 require("./config/passport");
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: 'GET, POST',
+  credentials: true,
+}))
+
 // Syncing our database and logging a message to the user upon success
 app.listen(PORT, function () {
   console.log(`==> 🌎  Listening on port ${PORT}. Visit http://localhost:${PORT} in your browser.`);
