3b0ca7190a188f14c057653b0bd32e70ba44864d
Gabriel Zsigmond <gaazsig@gmail.com>
2020-02-10 18:05:31 -0300

    google login and save token to localstorage

--- lendario/lendario-api/src/config/passport.js
+++ lendario/lendario-api/src/config/passport.js
@@ -1,10 +1,7 @@
 require('dotenv').config();
 const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env
-
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
-
-
 
 passport.serializeUser(function (user, done) {
   done(null, user);
@@ -12,17 +9,28 @@ passport.serializeUser(function (user, done) {
 passport.deserializeUser(function (user, done) {
   done(null, user);
 });
+
 passport.use(
   new GoogleStrategy(
     {
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3001/auth/google/callback"
+      callbackURL: "http://localhost:3001/auth/google/callback",
+      passReqToCallback   : true
     },
-    function (accessToken, refreshToken, profile, done) {
+    function (req , accessToken, refreshToken, profile, done) {
+      const {_json} = profile
+      const {
+        given_name,
+        family_name,
+        picture,
+        email,
+      } = _json
       const userData = {
-        email: profile.emails[0].value,
-        name: profile.displayName,
+        given_name,
+        family_name,
+        picture,
+        email,
         token: accessToken
       };
       done(null, userData);
--- lendario/lendario-api/src/routes/index.js
+++ lendario/lendario-api/src/routes/index.js
@@ -2,23 +2,64 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
-router.get('/', (req, res, next) => {
-  res.send("welcome to my api")
+const gcal = require('google-calendar');
+
+const defaultScope = [
+  'https://www.googleapis.com/auth/userinfo.profile',
+  'https://www.googleapis.com/auth/userinfo.email',
+  'https://www.googleapis.com/auth/calendar',
+];
+
+router.get('/',
+  passport.authenticate('google', { scope: defaultScope, failureRedirect: '/frown' }),
+  function (req, res) {
+    res.redirect('/success');
+  })
+
+router.get('/success', (req, res, next) => {
+  res.send("welcome")
 });
+
 
 /* GET Google Authentication API. */
 router.get(
   "/auth/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
+  passport.authenticate("google", { scope: defaultScope })
 );
+
 router.get(
   "/auth/google/callback",
   passport.authenticate("google", { failureRedirect: "/", session: false }),
-  function(req, res) {
-      var token = req.user.token;
-      res.redirect("http://localhost:3000?token=" + token);
+  (req, res) => {
+    const token = req.user.token;
+    console.log("user: ", req.user);
+
+    res.redirect("http://localhost:3000?token=" + token);
   }
 );
+
+router.post('/google/cal', (req, res) => {
+  const { token } = req.body;
+  console.log("hereeee:", token);
+  const google_calendar = new gcal.GoogleCalendar(token);
+
+  google_calendar.events.list('primary', (err, events) => {
+    // events.items.filter(event => {
+    //   if(new Date(event.start.dateTime) >= new Date()){
+    //     return event
+    //   }
+    // })
+// let result = []
+//     for (let i = 0; i < events.items.length; i++) {
+//       if (new Date(event.items[i].start.dateTime) >= new Date()) {
+//         result.push( event)
+//       }
+    // }
+
+    console.log("events ----->", events.items.length);
+  });
+  res.status(200).end
+})
 
 module.exports = {
   router
