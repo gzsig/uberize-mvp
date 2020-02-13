c1e7d2b056efd870fdcc61291a9018147eb740b1
Gabriel Zsigmond <gaazsig@gmail.com>
2020-02-13 11:58:11 -0300

    connecting to db and creating usermodel

---
+++ lendario/lendario-api/src/models/userModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  email: String,
+  given_name: String,
+  family_name: String,
+  picture: String,
+  refreshToken: String,
+  accessToken: String,
+
+},
+  {
+    timestamps: true
+  });
+
+const User = mongoose.model('User', userSchema);
+module.exports = { User };
