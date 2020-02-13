bdae6801f09f5b18aa3aceacc5cf213f057e6bd7
Gabriel Zsigmond <gaazsig@gmail.com>
2020-02-13 11:55:32 -0300

    atrempt to implement session

--- lendario/lendario-api/src/app.js
+++ lendario/lendario-api/src/app.js
@@ -1,27 +1,41 @@
-// Requiring necessary packages
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require('cors')
-// Requiring passport as we've configured it
-const passport = require("passport");
-// Setting up port 
-require('dotenv').config();
+const { passport } = require("./config/passport");
 const { PORT } = process.env
-// Creating express app and configuring middleware needed for authentication
 const { router } = require('./routes');
+const session = require('express-session');
+const connectMongo = require('connect-mongo');
+const MongoStore = connectMongo(session)
+const { mongooseConnect } = require('./resources')
 
+
+const mongoose = mongooseConnect();
 const app = express();
+
+app.use(session({
+  secret: "basic-auth-secret",
+  cookie: { maxAge: 60 * 60 * 24 * 1000 }, 
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    resave: true,
+    saveUninitialized: false,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// app.use(express.static("client/build"));
 app.use(passport.initialize());
-require("./config/passport");
+app.use(passport.session())
 
 app.use(cors({
   origin: 'http://localhost:3000',
   methods: 'GET, POST',
   credentials: true,
 }))
+
 
 // Syncing our database and logging a message to the user upon success
 app.listen(PORT, function () {
