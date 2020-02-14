09d2b5df4043a12278b642a9c176c4c5d162b22c
Gabriel Zsigmond <gaazsig@gmail.com>
2020-02-14 11:54:35 -0300

    saving crpt in local storage and sending as header in all requests

--- lendario/lendario-client/src/resources/axios.js
+++ lendario/lendario-client/src/resources/axios.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
-
+const { crpt } = window.localStorage;
 const server = axios.create({
   baseURL: 'http://localhost:3001/',
   withCredentials: true,
-})
+  headers: { authorization: crpt }
+});
 
 export default server;
