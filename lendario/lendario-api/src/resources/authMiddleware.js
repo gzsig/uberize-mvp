4636eb592358cc8a0a17771062098be1585bb98d
Gabriel Zsigmond <gaazsig@gmail.com>
2020-02-14 11:55:31 -0300

    added auth middleware

--- lendario/lendario-api/src/resources/authMiddleware.js
+++ lendario/lendario-api/src/resources/authMiddleware.js
@@ -1,12 +1,13 @@
 let jwt = require('jsonwebtoken');
 
 const authMiddleware = (request, response, next) => {
-  const token = request.headers['x-access-token'] || request.headers['authorization'];
-
+  const token =
+    request.headers['x-access-token'] || request.headers['authorization'];
+  console.log('header token:', token);
   if (token) {
     jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
       if (err) {
-        return request.json({
+        return response.json({
           success: false,
           message: 'Token is not valid'
         });
@@ -23,4 +24,6 @@ const authMiddleware = (request, response, next) => {
   }
 };
 
-module.exports = authMiddleware;
+module.exports = {
+  authMiddleware
+};
--- lendario/lendario-api/src/resources/index.js
+++ lendario/lendario-api/src/resources/index.js
@@ -1,5 +1,7 @@
 const { mongooseConnect } = require('./mongooseConnect');
+const { authMiddleware } = require('./authMiddleware');
 
 module.exports = {
   mongooseConnect,
-}
+  authMiddleware,
+};
