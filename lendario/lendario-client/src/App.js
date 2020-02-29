5a61efce56613e537551da20139db827643c8a4c
Gabriel Zsigmond <gaazsig@gmail.com>
2020-02-29 18:06:20 -0300

    starting new event page

--- lendario/lendario-client/src/App.js
+++ lendario/lendario-client/src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
-import { Login, Onboarding, CalendarView, Profile } from './containers';
+import {
+  Login,
+  Onboarding,
+  CalendarView,
+  Profile,
+  NewEvent
+} from './containers';
 import { Navbar, Loader } from './components';
 import AppProvider from './context/AppProvider';
 
@@ -14,6 +20,7 @@ function App() {
         <Route exact path='/le/onboarding' component={Onboarding} />
         <Route exact path='/le/calendario' component={CalendarView} />
         <Route exact path='/le/:username' component={Profile} />
+        <Route exact path='/le/:username/evento' component={NewEvent} />
         <Route exact path='/teste' component={Loader} />
       </Switch>
     </AppProvider>
---
+++ lendario/lendario-client/src/containers/NewEvent/NewEvent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import * as G from '../../resources/globalStyle';
+
+class NewEvent extends Component {
+  render() {
+    return <G.Wrapper>
+      <G.Frame>
+        Hello
+      </G.Frame>
+    </G.Wrapper>;
+  }
+}
+
+export default NewEvent;
