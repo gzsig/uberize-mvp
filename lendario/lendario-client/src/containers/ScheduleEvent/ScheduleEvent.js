2b4157087ddaff692913b4f9ca9d886390e90fb0
Gabriel Zsigmond <gaazsig@gmail.com>
2020-03-04 22:30:54 -0300

    getting dates on screen

--- lendario/lendario-client/src/containers/ScheduleEvent/ScheduleEvent.js
+++ lendario/lendario-client/src/containers/ScheduleEvent/ScheduleEvent.js
@@ -2,26 +2,44 @@ import React, { Component } from 'react';
 import server from '../../resources/axios';
 
 class ScheduleEvent extends Component {
-  // componentDidMount = () => {
-  //   server()
-  //     .get(`patient/appointment/${this.props.match.params.id}`)
-  //     .then(res => console.log(res))
-  //     .catch(err => console.log(err));
-  // };
+  state = {
+    slots: []
+  };
+  componentDidMount = () => {
+    server()
+      .post(`patient/appointment/${this.props.match.params.id}`)
+      .then(res => {
+        console.log(res);
+        this.setState({ slots: res.data.slots });
+      })
+      .catch(err => console.log(err));
+  };
   render() {
     return (
       <div>
         <p>ScheduleEvent</p>
-        <button
+        {this.state.slots.map((event, index) => {
+          return (
+            <div>
+              {new Date(event.start).toLocaleString('en-US', {
+                timeZone: 'America/Sao_Paulo'
+              })}
+              {new Date(event.end).toLocaleString('en-US', {
+                timeZone: 'America/Sao_Paulo'
+              })}
+            </div>
+          );
+        })}
+        {/* <button
           onClick={() => {
             server()
-              .get(`patient/appointment/${this.props.match.params.id}`)
+              .post(`patient/appointment/${this.props.match.params.id}`)
               .then(res => console.log(res))
               .catch(err => console.log(err));
           }}
         >
           click me
-        </button>
+        </button> */}
       </div>
     );
   }
