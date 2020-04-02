0fa7ed8f442bde1885ca3b3b8292b72eea753bc1
Gabriel Zsigmond <gaazsig@gmail.com>
2020-04-02 11:25:08 -0300

    added semantic ui

--- lendario/lendario-client/src/App.js
+++ lendario/lendario-client/src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import './App.css';
-import { Switch, Route } from 'react-router-dom';
+import React from "react";
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
 import {
   Login,
   Onboarding,
@@ -11,25 +12,33 @@ import {
   NewEvent,
   ScheduleEvent,
   ScheduleEventConfirm
-} from './containers';
-import { Navbar, Loader } from './components';
-import AppProvider from './context/AppProvider';
+} from "./containers";
+import { Navbar, Loader } from "./components";
+import AppProvider from "./context/AppProvider";
 
 function App() {
   return (
     <AppProvider>
       <Navbar />
       <Switch>
-        <Route exact path='/' component={Login} />
-        <Route exact path='/le/onboarding' component={Onboarding} />
-        <Route exact path='/le/calendario' component={CalendarView} />
-        <Route exact path='/le/:username' component={Profile} />
-        <Route exact path='/le/:username/evento' component={NewAppointment} />
-        <Route exact path='/le/:username/evento/:id' component={EditAppointment} />
-        <Route exact path='/novo/:username' component={NewEvent} />
-        <Route exact path='/novo/:username/:id' component={ScheduleEvent} />
-        <Route exact path='/novo/:username/:idappointment/confirmacao/:idtime' component={ScheduleEventConfirm} />
-        <Route exact path='/teste' component={Loader} />
+        <Route exact path="/" component={Login} />
+        <Route exact path="/le/onboarding" component={Onboarding} />
+        <Route exact path="/le/calendario" component={CalendarView} />
+        <Route exact path="/le/:username" component={Profile} />
+        <Route exact path="/le/:username/evento" component={NewAppointment} />
+        <Route
+          exact
+          path="/le/:username/evento/:id"
+          component={EditAppointment}
+        />
+        <Route exact path="/novo/:username" component={NewEvent} />
+        <Route exact path="/novo/:username/:id" component={ScheduleEvent} />
+        <Route
+          exact
+          path="/novo/:username/:idappointment/confirmacao/:idtime"
+          component={ScheduleEventConfirm}
+        />
+        <Route exact path="/teste" component={Loader} />
       </Switch>
     </AppProvider>
   );
